8bded13b6cfb0f53e3e99c5788a49bc8289ce800
carlincb <carlin.colleen@gmail.com>
2022-02-10 21:43:48 -0700

    Finished server.js.

---
+++ Develop/server/schemas/index.js
@@ -0,0 +1,8 @@
+const resolvers = require("./resolvers");
+const typeDefs = require("./typeDefs");
+
+module.exports = {
+  resolvers,
+  typeDefs,
+};
+
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,19 @@
+const {ApolloServer} = require('apollo-server-express');
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const {authMiddleware} = require('./utils/auth');
+const {resolvers, typeDefs} = require('./schemas');
+
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs, resolvers, context: authMiddleware
+})
+
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,8 +22,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
